eb1276a5a94486e35a196bdd88387147f26f0786
DoughNout <Khons177@hotmail.co.uk>
2013-12-14 17:50:57 +0000

    Midway through models

--- common/khons/coffee/CoffeeCore.java
+++ common/khons/coffee/CoffeeCore.java
@@ -1,6 +1,9 @@
 package khons.coffee;
 
+import khons.coffee.blocks.TileEntityCoffeeMachineBlock;
 import khons.coffee.proxy.CommonProxy;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -10,8 +13,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "coffee", name = "Coffe Mod", version = "0.0.2")
+@Mod(modid = "coffee", name = "Coffe Mod", version = "0.0.3")
 @NetworkMod(serverSideRequired = false, clientSideRequired = true)
 public class CoffeeCore {
 	
@@ -20,6 +25,8 @@ public class CoffeeCore {
 	
 	@SidedProxy(clientSide = "khons.coffee.proxy.ClientProxy", serverSide = "khons.coffee.proxy.CommonProxy")
 	public static CommonProxy proxy;
+	
+	public static Block coffeeMachine;
 	
 	public static Item Espresso; // Water and Coffee
 	public static Item EspressoDupio; // 2xEspresso
@@ -37,7 +44,24 @@ public class CoffeeCore {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent evt)
 	{
+		coffeeMachine = new TileEntityCoffeeMachineBlock(3773).setCreativeTab(CreativeTabs.tabBlock).setUnlocalizedName("coffeeMachine");
 		
+		GameRegistry.registerBlock(coffeeMachine, "coffeeMachine");
+		
+		LanguageRegistry.addName(coffeeMachine, "Coffee Machine");
+		
+		LanguageRegistry.addName(Americano, "Americano");
+		LanguageRegistry.addName(BlackEye, "Black eye");
+		LanguageRegistry.addName(CafeAuLait, "Cafe au lait");
+		LanguageRegistry.addName(DeadEye, "Dead eye");
+		LanguageRegistry.addName(Espresso, "Espresso");
+		LanguageRegistry.addName(EspressoDupio, "Espresso dupio");
+		LanguageRegistry.addName(EspressoLungo, "Espresso lungo");
+		LanguageRegistry.addName(EspressoRistretto, "Espresso ristretto");
+		LanguageRegistry.addName(Frappe, "Frappe");
+		LanguageRegistry.addName(Latte, "Latte");
+		LanguageRegistry.addName(MochaLatte, "Mocha Latte");
+		LanguageRegistry.addName(RedEye, "Red eye");
 	}
 	
 	public void init(FMLInitializationEvent evt)
---
+++ common/khons/coffee/blocks/TileEntityCoffeeMachineBlock.java
@@ -0,0 +1,45 @@
+package khons.coffee.blocks;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class TileEntityCoffeeMachineBlock extends BlockContainer {
+
+	public TileEntityCoffeeMachineBlock(int id, Material material) {
+		super(id, material);
+        this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+
+    //Make sure you set this as your TileEntity class relevant for the block!
+    @Override
+    public TileEntity createNewTileEntity(World world) {
+            return new TileEntityCoffeeMachineEntity();
+    }
+    
+    //You don't want the normal render type, or it wont render properly.
+    @Override
+    public int getRenderType() {
+            return -1;
+    }
+    
+    //It's not an opaque cube, so you need this.
+    @Override
+    public boolean isOpaqueCube() {
+            return false;
+    }
+    
+    //It's not a normal block, so you need this too.
+    public boolean renderAsNormalBlock() {
+            return false;
+    }
+    
+    //This is the icon to use for showing the block in your hand.
+    public void registerIcons(IconRegister icon) {
+            this.blockIcon = icon.registerIcon("coffee:machine");
+    }
+
+}
